8b8972bc8210eb02fe8342c3d3bd0cd90d9ec847
Sachin Ravi <sachinravi14@gmail.com>
2014-12-28 00:37:07 -0500

    Fixed split to supplement training set

--- tools/sachinr/split_by_id.cpp
+++ tools/sachinr/split_by_id.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
   ::google::InitGoogleLogging(argv[0]);
   
   if (argc < 4) {
-    LOG(ERROR) << "Usage: split original_file id_file loc1/file1 loc2/file2";
+    LOG(ERROR) << "Usage: split_by_id file_to_supplement file_to_split id_file loc1/file1 loc2/file2";
     return 1;
   }
   
@@ -41,10 +41,10 @@ int main(int argc, char *argv[]) {
   MDB_cursor* mdb_cursor;
 
   // Open db
-  LOG(INFO) << "Opening lmdb " << argv[1];
+  LOG(INFO) << "Opening lmdb " << argv[2];
   CHECK_EQ(mdb_env_create(&mdb_env), MDB_SUCCESS) << "mdb_env_create failed";
   CHECK_EQ(mdb_env_set_mapsize(mdb_env, 1099511627776), MDB_SUCCESS);  // 1TB
-  CHECK_EQ(mdb_env_open(mdb_env, argv[1], MDB_RDONLY, 0664),
+  CHECK_EQ(mdb_env_open(mdb_env, argv[2], MDB_RDONLY, 0664),
                   MDB_SUCCESS) << "mdb_env_open failed";
   CHECK_EQ(mdb_txn_begin(mdb_env, NULL, MDB_RDONLY, &mdb_txn), MDB_SUCCESS)
           << "mdb_txn_begin failed";
@@ -53,8 +53,13 @@ int main(int argc, char *argv[]) {
   CHECK_EQ(mdb_cursor_open(mdb_txn, mdb_dbi, &mdb_cursor), MDB_SUCCESS)
           << "mdb_cursor_open failed";
   CHECK_EQ(mdb_cursor_get(mdb_cursor, &mdb_key, &mdb_value, MDB_FIRST),
-                  MDB_SUCCESS); 
+                  MDB_SUCCESS);  
   
+  MDB_env* mdb_env_w0;
+  MDB_dbi mdb_dbi_w0;
+  MDB_txn* mdb_txn_w0;
+  MDB_cursor* mdb_cursor_w0;
+ 
   MDB_env* mdb_env_w1;
   MDB_dbi mdb_dbi_w1;
   MDB_txn* mdb_txn_w1;
@@ -63,10 +68,24 @@ int main(int argc, char *argv[]) {
   MDB_dbi mdb_dbi_w2;
   MDB_txn* mdb_txn_w2;
 
+  CHECK_EQ(mdb_env_create(&mdb_env_w0), MDB_SUCCESS) << "mdb_env_create failed";
+  CHECK_EQ(mdb_env_set_mapsize(mdb_env_w0, 1099511627776), MDB_SUCCESS)  // 1TB
+          << "mdb_env_set_mapsize failed";
+  CHECK_EQ(mdb_env_open(mdb_env_w0, argv[1], 0, 0664), MDB_SUCCESS)
+          << "mdb_env_open failed";
+  CHECK_EQ(mdb_txn_begin(mdb_env_w0, NULL, 0, &mdb_txn_w0), MDB_SUCCESS)
+          << "mdb_txn_begin failed";
+  CHECK_EQ(mdb_open(mdb_txn_w0, NULL, 0, &mdb_dbi_w0), MDB_SUCCESS)
+          << "mdb_open failed. Does the lmdb already exist? ";
+  CHECK_EQ(mdb_cursor_open(mdb_txn_w0, mdb_dbi_w0, &mdb_cursor_w0), MDB_SUCCESS)
+          << "mdb_cursor_open failed";
+  CHECK_EQ(mdb_cursor_get(mdb_cursor_w0, &mdb_key, &mdb_value, MDB_FIRST),
+                  MDB_SUCCESS);
+  
   CHECK_EQ(mdb_env_create(&mdb_env_w1), MDB_SUCCESS) << "mdb_env_create failed";
   CHECK_EQ(mdb_env_set_mapsize(mdb_env_w1, 1099511627776), MDB_SUCCESS)  // 1TB
           << "mdb_env_set_mapsize failed";
-  CHECK_EQ(mdb_env_open(mdb_env_w1, argv[3], 0, 0664), MDB_SUCCESS)
+  CHECK_EQ(mdb_env_open(mdb_env_w1, argv[4], 0, 0664), MDB_SUCCESS)
           << "mdb_env_open failed";
   CHECK_EQ(mdb_txn_begin(mdb_env_w1, NULL, 0, &mdb_txn_w1), MDB_SUCCESS)
           << "mdb_txn_begin failed";
@@ -76,18 +95,25 @@ int main(int argc, char *argv[]) {
   CHECK_EQ(mdb_env_create(&mdb_env_w2), MDB_SUCCESS) << "mdb_env_create failed";
   CHECK_EQ(mdb_env_set_mapsize(mdb_env_w2, 1099511627776), MDB_SUCCESS)  // 1TB
           << "mdb_env_set_mapsize failed";
-  CHECK_EQ(mdb_env_open(mdb_env_w2, argv[4], 0, 0664), MDB_SUCCESS)
+  CHECK_EQ(mdb_env_open(mdb_env_w2, argv[5], 0, 0664), MDB_SUCCESS)
           << "mdb_env_open failed";
   CHECK_EQ(mdb_txn_begin(mdb_env_w2, NULL, 0, &mdb_txn_w2), MDB_SUCCESS)
           << "mdb_txn_begin failed";
   CHECK_EQ(mdb_open(mdb_txn_w2, NULL, 0, &mdb_dbi_w2), MDB_SUCCESS)
           << "mdb_open failed. Does the lmdb already exist? ";
 
+  // Write entire first lmdb
+  int numAdds1 = 0, numAdds2 = 0;
+  do {
+    // Writo first file
+    write(numAdds1, mdb_dbi_w1, mdb_txn_w1, mdb_env_w1, mdb_key, mdb_value);  
+  } while(mdb_cursor_get(mdb_cursor_w0, &mdb_key, &mdb_value, MDB_NEXT)
+                  == MDB_SUCCESS);
+
   LOG(INFO) << "Reading ids...";
-  std::ifstream file(argv[2]);
+  std::ifstream file(argv[3]);
   string line;
   int curr = 0;
-  int numAdds1 = 0, numAdds2 = 0;
   int status = -1;
  
   while (getline(file, line)) {
