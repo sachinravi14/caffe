ffda7a83f3eeea447e8f8b051631fa0472ea5be6
Sachin Ravi <sachinravi14@gmail.com>
2015-03-05 15:33:55 -0500

    resetting accuracy layer back to standard

--- src/caffe/layers/accuracy_layer.cpp
+++ src/caffe/layers/accuracy_layer.cpp
@@ -10,18 +10,10 @@
 
 namespace caffe {
 
-const string PATH = "/home/sachinr/exp-data/week2/";
-const string FILE_PREFIX = "ids_with_thres_";
-const string FILE_FORMAT = ".txt";
-const double threshold = 0.125;
-
-template <typename Dtype>
-int AccuracyLayer<Dtype>::item_id = 0;
-
 template <typename Dtype>
 void AccuracyLayer<Dtype>::LayerSetUp(
   const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
-  top_k_ = 1;
+  top_k_ = this->layer_param_.accuracy_param().top_k();
 }
 
 template <typename Dtype>
@@ -40,77 +32,34 @@ void AccuracyLayer<Dtype>::Reshape(
 template <typename Dtype>
 void AccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     vector<Blob<Dtype>*>* top) {
-  Dtype num_correct = 0;
+  Dtype accuracy = 0;
   const Dtype* bottom_data = bottom[0]->cpu_data();
   const Dtype* bottom_label = bottom[1]->cpu_data();
   int num = bottom[0]->num();
   int dim = bottom[0]->count() / bottom[0]->num();
-
-  int actual_num_classified = 0;
   vector<Dtype> maxval(top_k_+1);
   vector<int> max_id(top_k_+1);
-
-  // Get softmax values
-  LayerParameter layer_param;
-  SoftmaxLayer<Dtype> layer(layer_param);
-  vector<Blob<Dtype>*> vals;
-  vals.push_back(bottom[0]);
-  vector<Blob<Dtype>*> prob;
-  Blob<Dtype>* blob(new Blob<Dtype>());
-  prob.push_back(blob);
-  layer.SetUp(vals, &prob);
-  layer.Forward(vals, &prob);
-
-  // Open file for writing threshold info 
-  std::ofstream file;
-  std::ostringstream s;
-  s << PATH << FILE_PREFIX << threshold << FILE_FORMAT;
-  file.open(s.str().c_str(), std::ios::app);
-
-  const Dtype* prob_data = prob[0]->cpu_data();
   for (int i = 0; i < num; ++i) {
-    // Calculate entrpy  
-    double entropy = 0.0;
+    // Top-k accuracy
+    std::vector<std::pair<Dtype, int> > bottom_data_vector;
     for (int j = 0; j < dim; ++j) {
-      entropy += -(prob_data[i * dim + j] * ((log(prob_data[i * dim + j])))/(log(2.0)));
-    } 
-    if (entropy < threshold) {
-      ++actual_num_classified;
-
-      // Top-k accuracy
-      std::vector<std::pair<Dtype, int> > bottom_data_vector;
-      for (int j = 0; j < dim; ++j) {
-        bottom_data_vector.push_back(
+      bottom_data_vector.push_back(
           std::make_pair(bottom_data[i * dim + j], j));
-      }
-      std::partial_sort(
+    }
+    std::partial_sort(
         bottom_data_vector.begin(), bottom_data_vector.begin() + top_k_,
         bottom_data_vector.end(), std::greater<std::pair<Dtype, int> >());
-      // check if true label is in top k predictions
-      for (int k = 0; k < top_k_; k++) {
-        if (bottom_data_vector[k].second == static_cast<int>(bottom_label[i])) {
-          ++num_correct;
-          break;
-        }
+    // check if true label is in top k predictions
+    for (int k = 0; k < top_k_; k++) {
+      if (bottom_data_vector[k].second == static_cast<int>(bottom_label[i])) {
+        ++accuracy;
+        break;
       }
-      
-      // Assume we predicted most likely label
-      file << item_id << "," << bottom_data_vector[0].second << "\n";   
     }
+  }
 
-    //LOG(INFO) << "Item id: " << item_id;
-    ++item_id;
-  }
-  LOG(INFO) << "Num Correct: " << num_correct;
-  LOG(INFO) << "Num Clasified: " << actual_num_classified;
-  if (actual_num_classified > 0) {
-    (*top)[0]->mutable_cpu_data()[0] = num_correct / actual_num_classified;
-  }
-  else {
-    (*top)[0]->mutable_cpu_data()[0] = 1.0;
-  }
-  
-  file.close();
+  // LOG(INFO) << "Accuracy: " << accuracy;
+  (*top)[0]->mutable_cpu_data()[0] = accuracy / num;
   // Accuracy layer should not be used as a loss function.
 }
 
