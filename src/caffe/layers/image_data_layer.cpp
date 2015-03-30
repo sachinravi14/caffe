865e98fd20ba0d51f6df4545d4e76bdbc4cbbbaa
Sachin Ravi <sachinravi14@gmail.com>
2015-03-30 16:30:10 -0400

    Fixing bad image detection in image_data_layer

--- src/caffe/layers/image_data_layer.cpp
+++ src/caffe/layers/image_data_layer.cpp
@@ -3,12 +3,15 @@
 #include <string>
 #include <utility>
 #include <vector>
+//#include <omp>
 
 #include "caffe/data_layers.hpp"
 #include "caffe/layer.hpp"
 #include "caffe/util/io.hpp"
 #include "caffe/util/math_functions.hpp"
 #include "caffe/util/rng.hpp"
+#include "time.h"
+#include <omp.h>
 
 namespace caffe {
 
@@ -43,6 +46,7 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
     ShuffleImages();
   }
   LOG(INFO) << "A total of " << lines_.size() << " images.";
+  LOG(ERROR) << "A total of " << lines_.size() << " images.";
 
   lines_id_ = 0;
   // Check if we would need to randomly skip a few data points
@@ -81,6 +85,7 @@ void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
   this->datum_height_ = datum.height();
   this->datum_width_ = datum.width();
   this->datum_size_ = datum.channels() * datum.height() * datum.width();
+  LOG(ERROR) << "Data layer initialized!";
 }
 
 template <typename Dtype>
@@ -94,6 +99,7 @@ void ImageDataLayer<Dtype>::ShuffleImages() {
 template <typename Dtype>
 void ImageDataLayer<Dtype>::InternalThreadEntry() {
   Datum datum;
+  vector<Datum> vdatum(1000, Datum());
   CHECK(this->prefetch_data_.count());
   Dtype* top_data = this->prefetch_data_.mutable_cpu_data();
   Dtype* top_label = this->prefetch_label_.mutable_cpu_data();
@@ -103,20 +109,81 @@ void ImageDataLayer<Dtype>::InternalThreadEntry() {
   const int new_width = image_data_param.new_width();
 
   // datum scales
+  //clock_t loadtime = 0;
+  //clock_t trantime = 0;
   const int lines_size = lines_.size();
+
+  
+  LOG(INFO) << "read index start";  
+  vector<int> current_id;
+  current_id.assign(batch_size, 0);
+  for (int item_id = 0; item_id < batch_size; ++item_id)
+  {
+    CHECK_GT(lines_size, lines_id_);
+    current_id[item_id] = lines_id_;
+    lines_id_++;
+    if (lines_id_ >= lines_size) {
+      // We have reached the end. Restart from the first.
+      DLOG(INFO) << "Restarting data prefetching from start.";
+      lines_id_ = 0;
+      if (this->layer_param_.image_data_param().shuffle()) {
+        ShuffleImages();
+      }
+    }
+  }
+
+  //omp_set_dynamic(0);     // Explicitly disable dynamic teams
+  //omp_set_num_threads(4); // Use 4 threads for all consecutive parallel regions
+
+  //for (int i = 0; i<batch_size; i++)
+  //{
+  //  LOG(ERROR) << current_id[i];
+  //}
+
+  LOG(INFO) << "read data start";  
+  #pragma omp parallel num_threads(15)
+  {
+    #pragma omp for
+    for (int item_id = 0; item_id < batch_size; ++item_id)
+    {
+      int id = current_id[item_id];
+      // LOG(ERROR) << item_id << ":" << id << ":" << lines_[id].first;
+      while (!ReadImageToDatum(lines_[id].first,
+            lines_[id].second,
+            new_height, new_width, &(vdatum[item_id]))) {
+        LOG(ERROR) << id << ": "<< lines_[id].first << " (ERRORLOAD)";
+      }
+      this->data_transformer_.Transform(item_id, vdatum[item_id], this->mean_, top_data);
+      top_label[item_id] = vdatum[item_id].label();
+    }
+  //  LOG(ERROR) << "threads="<<omp_get_num_threads()<<endl;
+  }
+  LOG(INFO) << "read data finish";  
+  
+
+  /*
   for (int item_id = 0; item_id < batch_size; ++item_id) {
     // get a blob
+    //clock_t t1 = clock();
     CHECK_GT(lines_size, lines_id_);
     if (!ReadImageToDatum(lines_[lines_id_].first,
           lines_[lines_id_].second,
           new_height, new_width, &datum)) {
+    	LOG(ERROR) << lines_id_ << ": "<< lines_[lines_id_].first;
+    	lines_id_++;
       continue;
     }
+    //loadtime += clock() - t1;
 
+    
     // Apply transformations (mirror, crop...) to the data
+    //t1 = clock();
     this->data_transformer_.Transform(item_id, datum, this->mean_, top_data);
+    //trantime += clock() - t1;
 
     top_label[item_id] = datum.label();
+
+
     // go to the next iter
     lines_id_++;
     if (lines_id_ >= lines_size) {
@@ -128,6 +195,9 @@ void ImageDataLayer<Dtype>::InternalThreadEntry() {
       }
     }
   }
+  */
+  
+  //printf("Loadtime: %d; Trantime: %d\n", loadtime, trantime);
 }
 
 INSTANTIATE_CLASS(ImageDataLayer);
